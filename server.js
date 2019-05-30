3d72cf9771537c093e487933c74406125ac4e83d
Nyapal Gatkuoth <nyapal14@gmail.com>
2019-05-30 13:30:11 -0700

    users can view list of post

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const expressValidator = require('express-validator');
 const app = express();
 const port = 3000
 
-require('./data/reddit-db');
-
 mongoose.connect('mongodb://localhost/reddit2', { useNewUrlParser: true });
 
 app.use(bodyParser.json())
@@ -16,45 +14,16 @@ app.use(expressValidator())
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
-// let posts = [
-//   { comment: 'first post' },
-//   { comment: 'second post' } 
-// ]
 
-const Post = mongoose.model('Post', {
-  title: String,
-  body: String
-})
+// const Post = mongoose.model('Post', {
+//   title: String,
+//   body: String
+// })
 
-// INDEX
-app.get('/', (req, res) => {
-  Post.find()
-    .then(posts => {
-      res.render('posts-index', {posts: posts})
-    })
-    .catch(err => {
-      console.log(err)
-    })
-})
-
-// NEW POST FORM
-app.get('/posts/new', (req, res) => {
-  res.render('posts-new', {})
-})
-
-// CREATE
-app.post('/posts', (req, res) => {
-  // Post.create(req.body).then((post) => {
-  //   console.log(post)
-  //   res.redirect('/')
-  // }).catch((err) => {
-  //   console.log(err.message)
-  // })
-  const post = new Post(req.body)
-
-  post.save((err, post) => {
-    return res.redirect(`/`)
-  })
-})
+require('./controllers/posts.js')(app);
+require('./data/reddit-db');
+// require('./controllers/comments.js')(app);
+// require('./controllers/auth.js')(app);
+// require('./controllers/replies.js')(app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
